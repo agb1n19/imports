e9c7ceb76392545efe73e03cd42d6531a08f6415
agb1n19 <58887711+agb1n19@users.noreply.github.com>
2020-09-18 20:17:09 +0100

    Add files via upload

---
+++ q/meta/15_2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l nodes=16:ppn=16
+#PBS -m ae -M agb1n19@soton.ac.uk
+#PBS -l qos=standard
+module load singularity/3.2.0
+cd
+singularity exec plnr_cmaes.simg ~/cc/meta/15_2.sh
